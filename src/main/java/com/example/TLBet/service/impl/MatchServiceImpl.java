99d8f5a4e3f28b94fb2795cd02c78c87a6753170
Ivo Krastev <IKrastev@technologica.com>
2024-06-07 15:20:31 +0300

    fix returned goals after bet - getAllMatches

--- src/main/java/com/example/TLBet/service/impl/MatchServiceImpl.java
+++ src/main/java/com/example/TLBet/service/impl/MatchServiceImpl.java
@@ -43,38 +43,47 @@ public class MatchServiceImpl implements MatchService {
 
     @Override
     public List<MatchResultView> getLastRoundMatches(String username, Round round, List<Bet> createdBets) {
-
         List<Match> lastRoundMatches = matchRepository.getLastRoundMatches(round.getId());
         List<MatchResultView> result = new ArrayList<>();
         lastRoundMatches
                 .forEach(match -> {
                     Bet optionalBet = createdBets.stream().filter(x -> x.getMatch().getId() == match.getId()).findFirst().orElse(null);
                     MatchStatus status = MatchStatus.PLAYABLE;
-                    if(optionalBet != null){
-                        if(match.getAwayTeamGoals() == null || match.getHomeTeamGoals() == null){
+
+                    Integer awayTeamGoals = null;
+                    Integer homeTeamGoals = null;
+
+                    //Потребителят е направил залог
+                    if (optionalBet != null) {
+                        //Попълваме голововете от залога
+                        awayTeamGoals = optionalBet.getAwayTeamGoals();
+                        homeTeamGoals = optionalBet.getHomeTeamGoals();
+
+                        if (match.getAwayTeamGoals() == null || match.getHomeTeamGoals() == null) {
+                            //Резултатът от мача все още не е попълнен в системата(мачът не е завършил)
                             status = MatchStatus.AWAITING_RESULT;
-                        }else if(match.getAwayTeamGoals().equals(optionalBet.getAwayTeamGoals()) && match.getHomeTeamGoals().equals(optionalBet.getHomeTeamGoals())){
+                        } else if (match.getAwayTeamGoals().equals(optionalBet.getAwayTeamGoals()) && match.getHomeTeamGoals().equals(optionalBet.getHomeTeamGoals())) {
+                            //Точен резултат
                             status = MatchStatus.EXACT_WIN;
-                        } else if((match.getHomeTeamGoals() > match.getAwayTeamGoals()  && optionalBet.getHomeTeamGoals() > optionalBet.getAwayTeamGoals() )
-                                || (match.getHomeTeamGoals() < match.getAwayTeamGoals()  && optionalBet.getHomeTeamGoals() < optionalBet.getAwayTeamGoals())
-                                ||(match.getHomeTeamGoals().equals(match.getAwayTeamGoals()) && Objects.equals(optionalBet.getHomeTeamGoals(), optionalBet.getAwayTeamGoals()))){
+                        } else if ((match.getHomeTeamGoals() > match.getAwayTeamGoals() && optionalBet.getHomeTeamGoals() > optionalBet.getAwayTeamGoals())
+                                || (match.getHomeTeamGoals() < match.getAwayTeamGoals() && optionalBet.getHomeTeamGoals() < optionalBet.getAwayTeamGoals())
+                                || (match.getHomeTeamGoals().equals(match.getAwayTeamGoals()) && Objects.equals(optionalBet.getHomeTeamGoals(), optionalBet.getAwayTeamGoals()))) {
+                            //Потребителят е познал знака 1/X/2
                             status = MatchStatus.WON;
-                        }else {
+                        } else {
+                            //Грешна прогноза
                             status = MatchStatus.LOST;
                         }
-                    }
-                    else if(Instant.now().isAfter(match.getStartTime())){
+                    } else if (Instant.now().isAfter(match.getStartTime())) {
+                        //Изтекло време за игра
                         status = MatchStatus.EXPIRED;
                     }
 
-                    Integer awayTeamGoals = match.getAwayTeamGoals();
-                    Integer homeTeamGoals = match.getHomeTeamGoals();
-                    if(status == MatchStatus.PLAYABLE){
+                    if (status == MatchStatus.PLAYABLE) {
+                        //Слагаме голове по подразбиране ако потребителя не е заложил на мача
                         awayTeamGoals = 0;
                         homeTeamGoals = 0;
-
                     }
-
 
                     MatchResultView matchResultView = MatchResultView.builder()
                             .id(match.getId())
