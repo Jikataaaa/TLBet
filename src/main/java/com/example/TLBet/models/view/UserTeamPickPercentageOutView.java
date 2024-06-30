e40796d9e2c8534fe055486c2f40ed5ac32d2c1f
Ivo Krastev <IKrastev@technologica.com>
2024-06-30 14:41:10 +0300

    feat: Update stats component buttons
    Update the labels and click event handlers for the buttons in the stats component HTML file to match the corresponding stats types.

--- src/main/java/com/example/TLBet/models/view/UserTeamPickPercentageOutView.java
+++ src/main/java/com/example/TLBet/models/view/UserTeamPickPercentageOutView.java
@@ -12,7 +12,6 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Builder
 public class UserTeamPickPercentageOutView {
-
     private String teamName;
     private BigDecimal teamPickPercentage;
     private Long teamPickCount;
